a72ee3e074b05b79121ed8e8aa35feb350c1ee27
robertadriang <58529493+robertadriang@users.noreply.github.com>
2022-01-09 23:31:25 +0200

    [CHANGE] Refactoring duplicate functions [CHANGE] Eliminated test code [ADD] See queue capacity

--- FindMissingBytes.py
+++ FindMissingBytes.py
@@ -2,7 +2,7 @@
 # (maxim “x” bytes de la finalul arhivei originale lipsesc).
 # Tool-ul va gasi fisierul original din arhiva
 # ( in momentul in care se va despacheta fisierul din arhiva va avea acelasi hash ca cel primit la input )
-# Pentru rezolvarea acestei probleme este imperativ folosirea oricarei forme de paralelizare (
+# Pentru rezolvarea acestei probleme este imperativa folosirea oricarei forme de paralelizare (
 #
 # multi threding/multiuprocessing / multi system / etc )
 # INPUT:
@@ -20,41 +20,47 @@ import zipfile
 from multiprocessing import Process, Queue, Lock,Pipe
 from queue import Empty
 import traceback
-import time
 
-def trim_file(file,x):
+
+def trim_file(file,x,save_bits=False):
     while True:
         try:
             f = open(file, 'r+b')
             f_size = f.seek(0, 2)
             f.seek(f_size - x, 0)
-            # removed=f.read(x)
-            # print(removed)
-            # print(int.from_bytes(removed,byteorder='big'))
-            # f.seek(f_size - x, 0)
+            if save_bits:
+                removed = f.read(x)
+                f.seek(f_size - x, 0)
             f.truncate()
             f.close()
         except Exception as e:
-            print("An error occured in trim_ile")
+            print("An error occured in trim_file")
             print(e)
-            #traceback.print_exc()
             continue
+        if save_bits:
+            return file,removed
         return file
 
 
-def trim_archive(archive,x,copy=False,c_name='truncated_archive'):
+def trim_archive(archive,x,copy=False,c_name='truncated_archive',save_bits=False):
     if copy==True:
         copy_name=f"{c_name}.{archive.split('.')[-1]}"
         shutil.copyfile(archive,copy_name) #create a copy of the archive
+        if save_bits:
+            return trim_file(copy_name,x,save_bits)
         return trim_file(copy_name,x)
     return trim_file(archive,x)
 
 
-def get_hash(file,hash_type):
-    m=hashlib.new(hash_type)
+def get_hash_unopened_file(file, hash_type):
     with open(file, 'rb') as f:
-        while chunk := f.read(1024):
-            m.update(chunk)
+        return get_hash_opened_file(f, hash_type)
+
+
+def get_hash_opened_file(file, hash_type):
+    m=hashlib.new(hash_type)
+    while chunk := file.read(1024):
+        m.update(chunk)
     return m.hexdigest()
 
 
@@ -87,34 +93,23 @@ def append_bits_to_file(file,bits):
     return len(bits)
 
 
-def get_hash_zip(file,hash_type):
-    m=hashlib.new(hash_type)
-    while chunk := file.read(1024):
-        m.update(chunk)
-    return m.hexdigest()
-
-
-#recompose_file(corrupted_archive,bits_missing,file_name,file_hash,hash_method)
 def recompose_file(archive,bytes_to_add,file_name,file_hash,hash_method):
     added_bytes_length = append_bits_to_file(archive, bytes_to_add)
     try:
         z = zipfile.ZipFile(archive)
-        # print(f"{file_name}")
-        # print(file_name in z.namelist())
         f = z.open(file_name)
-        computed_hash = get_hash_zip(f, hash_method)
+        computed_hash = get_hash_opened_file(f, hash_method)
         if computed_hash==file_hash:
             return True
         return False
     except Exception as e:
         ## TODO check other exception type
-
         ### BadZipFile when the archive is corrupted
         ### Errno 22 when the archive is valid but the file inside is corrupted
         if type(e).__name__ == 'BadZipFile' or '[Errno 22]' in str(e):
             pass
         else:
-            print("@@@@ NEW ERROR FOUND @@@")
+            print("@@@@ NEW ERROR FOUND @@@@")
             print(e)
             print(archive)
             traceback.print_exc()
@@ -123,48 +118,39 @@ def recompose_file(archive,bytes_to_add,file_name,file_hash,hash_method):
         trim_archive(archive, added_bytes_length)
 
 
-
 def producer(queue,lock,producer_number,elements_per_producer):
     with lock:
-        print(f'Starting producer {os.getpid()}')
-    #queue.cancel_join_thread()
-    producer_generator=byte_generator(elements_per_producer*producer_number,elements_per_producer*(producer_number+1))
-    print(elements_per_producer*producer_number,elements_per_producer*(producer_number+1))
+        print(f'Starting producer with PID {os.getpid()}')
+    producer_start=elements_per_producer*producer_number
+    producer_stop=elements_per_producer*(producer_number+1)
+    producer_generator=byte_generator(producer_start,producer_stop)
+    print(f"Limits of the producer with PID {os.getpid()}:[{producer_start},{producer_stop}]")
     for i in range(elements_per_producer):
-
-        element=next(producer_generator)
-        # if element == b'\x03\x00\x00':
-        #     print("Written in queue")
-        # print(element)
-        queue.put(element)
-        #queue.put(next(producer_generator))
-        # with lock:
-        #     print(f"{os.getpid()} added {element} to the queue")
-        #     print(f"{queue.qsize()}")
+        queue.put(next(producer_generator))
     with lock:
-        print(f'Closing producer {os.getpid()}')
+        print(f'Closing producer {os.getpid()} after all elements were added to the queue')
 
 
 def consumer(queue,lock,pipe_conn,corrupted_archive,file_name,file_hash,hash_method):
     with lock:
-        print(f'Starting consumer {os.getpid()}')
+        print(f'Starting consumer with PID {os.getpid()}')
 
+    ### Checks if we found a solution on this thread
     sent_value=0
+    QUEUE_TIMEOUT=3
+    elements_processed=0
 
     while not queue.empty():
         try:
-            r_value=queue.get(timeout=1)
-            # print(r_value)
-            # time.sleep(2)
+            r_value=queue.get(timeout=QUEUE_TIMEOUT)
+            elements_processed+=1
+            if elements_processed%500==0:
+                with lock:
+                    print(f"Consumer with PID {os.getpid()} processed f{elements_processed} elements. Queue remaining size:{queue.qsize()}")
             response=recompose_file(corrupted_archive,r_value,file_name,file_hash,hash_method)
             if response:
-                print(f"{os.getpid()} found the file after adding:",r_value)
-                # z = zipfile.ZipFile(corrupted_archive)
-                # f = z.open(file_name)
-                # f.seek(0)
-                # print("File content:")
-                # print(f.read())
                 with lock:
+                    print(f"Consumer with PID {os.getpid()} found the file after adding:",r_value)
                     print(f'{os.getpid()} tries to send correct bytes missing')
                 pipe_conn.send(r_value)
                 sent_value=1
@@ -177,31 +163,36 @@ def consumer(queue,lock,pipe_conn,corrupted_archive,file_name,file_hash,hash_met
 
         ### When we fail to extract an element from the queue
         except Empty:
-            print(f"{os.getpid()} found that the Queue was empty")
+            print(f"Consumer with PID {os.getpid()} found that the Queue was empty")
             if sent_value==0:
                 with lock:
-                    print(f'{os.getpid()} tries to send not found signal in Empty')
+                    print(f'Consumer with PID {os.getpid()} tries to send Not found signal (from exc)')
                 pipe_conn.send("Not found")
             else:
                 with lock:
-                    print(f'{os.getpid()} already sent results through pipe ')
+                    print(f'Consumer with PID {os.getpid()} already sent results through pipe ')
     if sent_value == 0:
         with lock:
-            print(f'{os.getpid()} tries to send not found signal after while')
+            print(f'Consumer with PID {os.getpid()} tries to send Not found signal (after while)')
         pipe_conn.send("Not found")
     return 0
 
 
 if __name__ == '__main__':
-    archive_name="./the.zip"
-    file_name='LoremIpsum.txt'
-    bits_missing=2
+    archive_name="./schemadownloader_1.0-windows.zip"
+    file_name='README.txt'
+    bits_missing=1
     hash_method='md5'
-    file_hash=get_hash(file_name,hash_method)
+
+    file_hash=get_hash_unopened_file(file_name, hash_method)
 
     queue=Queue()
     lock=Lock()
-    elements_to_be_added=256**bits_missing
+
+    number_of_missing_bytes=1
+
+
+    elements_to_be_added=256**(bits_missing)
     numbers_of_producers=4
     elements_per_producer=int(elements_to_be_added/numbers_of_producers) ### TODO: Check that numbers divide correctly
     numbers_of_consumers=4
@@ -211,13 +202,17 @@ if __name__ == '__main__':
     consumers=[]
     pipe_list=[]
 
-    main_corrupted_archive=trim_archive(archive_name,bits_missing,copy=True,c_name=f'MAIN_truncated_archive')
+    main_corrupted_archive,removed_bits=trim_archive(archive_name,bits_missing,copy=True,c_name=f'MAIN_truncated_archive',save_bits=True)
+    print("Bits that were removed:",removed_bits)
+    print("Generator value for the removed part:",int.from_bytes(removed_bits,byteorder='big'))
 
+    ### Create producers processes
     for i in range(numbers_of_producers):
         producers.append(Process(target=producer,args=(queue,lock,i,elements_per_producer)))
 
+    ### Create consumers processes
     for i in range(numbers_of_consumers):
-        corrupted_archive=trim_archive("./the.zip",bits_missing,copy=True,c_name=f'tr_{i}')
+        corrupted_archive=trim_archive(archive_name,bits_missing,copy=True,c_name=f'tr_{i}')
         parent_conn,child_conn=Pipe()
         pipe_list.append(parent_conn)
         c=Process(target=consumer,args=(queue,lock,child_conn,corrupted_archive,file_name,file_hash,hash_method))
@@ -230,17 +225,18 @@ if __name__ == '__main__':
         c.start()
 
     for p in producers:
-        print(p.join(),"Producer left")
+        p.join()
+        print(f"Producer {p} finished the job")
 
     with lock:
-        print(f'I try to read values from children')
-
+        print(f'Main thread tries to gather results from processes')
     processes_responses=[x.recv() for x in pipe_list]
 
     for c in consumers:
-        print(c.join(),"Consumer left")
+        c.join()
+        print(f"Consumer {c} finished the job")
 
-    print(processes_responses)
+    print("Results from processes:",processes_responses)
     for response in processes_responses:
         if response!='Not found':
             append_bits_to_file(main_corrupted_archive, response)
@@ -252,5 +248,7 @@ if __name__ == '__main__':
             print("File content:")
             print(f.read())
             print("Done!")
-            break
+            exit(0)
+    print("Failed to unpack.")
+    exit(-1)
 
